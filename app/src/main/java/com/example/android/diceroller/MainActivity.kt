30aa90bdfb37f5fc6214c936565ff0cc91ef6a61
luisina <marconi.luisina@gmail.com>
2024-05-10 15:56:29 -0300

    showing dice image

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -21,6 +21,7 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.diceroller.databinding.ActivityMainBinding
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -29,11 +30,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val rollButton: Button = binding.rollButton
-        rollButton.text = "Let's Roll"
-        rollButton.setOnClickListener(){
-            Toast.makeText(this, "Toast", Toast.LENGTH_SHORT).show()
+        rollButton.setOnClickListener() {
+            rollDice()
         }
 
+    }
 
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val diceImageView = binding.diceImage
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImageView.setImageResource(drawableResource)
     }
 }
