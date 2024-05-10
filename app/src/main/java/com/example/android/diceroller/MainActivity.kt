80faceded38b4b73970406f186fd69085428eeb7
luisina <marconi.luisina@gmail.com>
2024-05-10 15:36:32 -0300

    adding button toast

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -17,12 +17,23 @@
 package com.example.android.diceroller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.android.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val rollButton: Button = binding.rollButton
+        rollButton.text = "Let's Roll"
+        rollButton.setOnClickListener(){
+            Toast.makeText(this, "Toast", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
